5e02ba89d74243f75051c8ed320ec7355b5e4943
Maverick Coryell <maverick.coryell@gmail.com>
2021-02-19 13:39:11 -0500

    Refactored project structure into a more usable state.

--- src/main/java/com/modelbox/initialStagePrep.java
+++ src/main/java/com/modelbox/initialStagePrep.java
@@ -14,7 +14,7 @@ public class initialStagePrep extends Application {
         dashboardController dashController = new dashboardController();
         createAccountController createController = new createAccountController();
 
-        Boolean loggedIn = true;
+        Boolean loggedIn = false;
 
         if (loggedIn) {
             FXMLLoader dashboardLoader = new FXMLLoader();
