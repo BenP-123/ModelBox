fb797437cbc03adf24c03b85a36c70a2e987a3ec
Maverick Coryell <maverick.coryell@gmail.com>
2021-02-28 12:41:16 -0500

    Rotate and zoom control added to the preview pop-up.

--- src/main/java/com/modelbox/controllers/myModelsController.java
+++ src/main/java/com/modelbox/controllers/myModelsController.java
@@ -5,7 +5,10 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Camera;
+import javafx.scene.Group;
 import javafx.scene.Parent;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
@@ -15,6 +18,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
@@ -137,22 +141,31 @@ public class myModelsController {
             TriangleMesh currentModelMesh = loginController.dashboard.stlImporter.getImport();
             MeshView currentModelMeshView = new MeshView(currentModelMesh);
 
-            // Set the id of the previewModelAnchorPane to be equal to the modelName
-            loginController.dashboard.previewPopUpView.previewModelAnchorPane.setId(currentModel.getId());
+            // Set the id of the previewModelSubScene to be equal to the modelName
+            loginController.dashboard.previewPopUpView.previewModelSubScene.setId(currentModel.getId());
 
             // Set the modelNameText and modelTypeText labels
             loginController.dashboard.previewPopUpView.modelNameText.setText(FilenameUtils.removeExtension(currentModel.getId()));
             loginController.dashboard.previewPopUpView.modelTypeText.setText(FilenameUtils.getExtension(currentModel.getId()).toUpperCase());
 
-            // Add the mesh to the preview pop-up group for manipulation
-            loginController.dashboard.previewPopUpView.previewModelGroup.getChildren().add(currentModelMeshView);
+            // Add the mesh to the preview pop-up group
+            Group previewModelGroup = new Group(currentModelMeshView);
 
-            // FIXME: Make the model so that it can be manipulated in 3D space
-            // Position the model in the center of the model anchor pane
-            loginController.dashboard.previewPopUpView.previewModelGroup.setTranslateX(
-                    loginController.dashboard.previewPopUpView.previewModelAnchorPane.getWidth());
-            loginController.dashboard.previewPopUpView.previewModelGroup.setTranslateY(
-                    loginController.dashboard.previewPopUpView.previewModelAnchorPane.getHeight());
+            // Add the group to the sub-scene for manipulation
+            loginController.dashboard.previewPopUpView.previewModelSubScene.setRoot(previewModelGroup);
+
+            // Add the camera to the sub-scene
+            Camera camera = new PerspectiveCamera();
+            loginController.dashboard.previewPopUpView.previewModelSubScene.setCamera(camera);
+
+            // Position the model in the center of the sub-scene
+            loginController.dashboard.previewPopUpView.previewModelSubScene.widthProperty().bind(loginController.dashboard.previewPopUpView.previewModelAnchorPane.widthProperty());
+            loginController.dashboard.previewPopUpView.previewModelSubScene.heightProperty().bind(loginController.dashboard.previewPopUpView.previewModelAnchorPane.heightProperty());
+            previewModelGroup.translateXProperty().bind(loginController.dashboard.previewPopUpView.previewModelSubScene.widthProperty().divide(2.0));
+            previewModelGroup.translateYProperty().bind(loginController.dashboard.previewPopUpView.previewModelSubScene.heightProperty().divide(2.0));
+
+            // Make the model so that it can be manipulated in 3D space (rotated)
+            loginController.dashboard.previewPopUpView.initMouseControl(previewModelGroup, loginController.dashboard.previewPopUpView.previewModelSubScene, (Stage) myModelsAnchorPane.getScene().getWindow());
 
             // Actually launch the preview pop-up
             myModelsAnchorPane.getChildren().add(previewRoot);
--- src/main/java/com/modelbox/controllers/previewPopUpController.java
+++ src/main/java/com/modelbox/controllers/previewPopUpController.java
@@ -1,20 +1,32 @@
 package com.modelbox.controllers;
 
 import com.modelbox.databaseIO.modelsIO;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.SubScene;
 import javafx.scene.control.Button;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.scene.transform.Rotate;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class previewPopUpController {
 
-    @FXML public Group previewModelGroup;
+    private double anchorX, anchorY;
+    private double anchorAngleX = 0;
+    private double anchorAngleY = 0;
+    private final DoubleProperty angleX = new SimpleDoubleProperty(0);
+    private final DoubleProperty angleY = new SimpleDoubleProperty(0);
+
     @FXML public AnchorPane previewModelAnchorPane;
     @FXML public Text modelNameText;
     @FXML public Text modelTypeText;
+    @FXML public SubScene previewModelSubScene;
 
 
     @FXML
@@ -31,5 +43,33 @@ public class previewPopUpController {
                 new FileChooser.ExtensionFilter("STL File","*.stl"));
         fileSaver.setTitle("Save 3D Model");
         modelsIO.downloadModelFile(currentModel.getId(), (fileSaver.showSaveDialog(loginController.dashboard.dashboardAnchorPane.getScene().getWindow())).toPath());
+    }
+
+    public void initMouseControl(Group group, SubScene scene, Stage stage) {
+        Rotate xRotate;
+        Rotate yRotate;
+        group.getTransforms().addAll(
+                xRotate = new Rotate(0, Rotate.X_AXIS),
+                yRotate = new Rotate(0, Rotate.Y_AXIS)
+        );
+        xRotate.angleProperty().bind(angleX);
+        yRotate.angleProperty().bind(angleY);
+
+        scene.setOnMousePressed(event -> {
+            anchorX = event.getSceneX();
+            anchorY = event.getSceneY();
+            anchorAngleX = angleX.get();
+            anchorAngleY = angleY.get();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
+            angleY.set(anchorAngleY + anchorX - event.getSceneX());
+        });
+
+        stage.addEventHandler(ScrollEvent.SCROLL, event -> {
+            double delta = event.getDeltaY();
+            group.translateZProperty().set(group.getTranslateZ() + delta);
+        });
     }
 }
